79456ab87831f3b9dd377802eb95d96623e2bdef
Nicholas Leung <nleungky@gmail.com>
2020-03-28 20:20:21 -0700

    Updates

---
+++ precache-manifest.e40eff16f3f78cd388ed61ece55a21bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2867049c8cfaf78cca0be9351b8c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2348347c24de4107c92",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35d8e3e0252822261365",
+    "url": "/static/js/2.105b5654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.105b5654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2348347c24de4107c92",
+    "url": "/static/js/main.f9b904e5.chunk.js"
+  },
+  {
+    "revision": "5b9ebca0577708eb86c0",
+    "url": "/static/js/runtime-main.0b6d89bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
